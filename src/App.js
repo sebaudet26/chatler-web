8142672bdb065fe9dafefda997dc5621fe9604f4
Sébastien Audet <seb@Sebastiens-MacBook-Pro.local>
2017-12-06 13:38:56 -0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import Routes from "./Routes";
+import "./App.css";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Routes />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import asyncComponent from "./components/AsyncComponent";
+
+const AsyncHome = asyncComponent(() => import("./containers/Home"));
+const AsyncAbout = asyncComponent(() => import("./containers/About"));
+const AsyncNotFound = asyncComponent(() => import("./containers/NotFound"));
+
+export default ({ childProps }) =>
+  <Switch>
+    <Route path="/" exact component={AsyncHome} />
+    <Route path="/about" exact component={AsyncAbout} />
+    { /* Finally, catch all unmatched routes */ }
+    <Route component={AsyncNotFound} />
+  </Switch>;
