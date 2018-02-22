ae06cf44025efb1b2d6e3beffa195a5642bb88ac
Sébastien Audet <seb@Sebastiens-MacBook-Pro.local>
2018-02-21 21:55:51 -0500

    Company Page

--- src/containers/Company/index.js
+++ src/containers/Company/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Request from '../../components/Request/';
 import './Company.css';
+import WorkImg from '../../lib/img/work.svg';
+import RocketIcon from '../../lib/icons/rocket.svg';
+import LogoIcon from '../../lib/icons/chat-icon.svg';
 
 class About extends Component {
   render() {
@@ -23,23 +26,29 @@ class About extends Component {
             <div className="row">
               <div className="col-sm-6">
                 <div className="bubble-container left">
+                  <div className="img-icon green">
+                    <img src={RocketIcon} alt="" />
+                  </div>
                   <h2>We are a young startup from Montreal.</h2>
                   <p>At Chatler, we make it our mission to put businesses and customers in the same conversation. We are a team of engineers and designers who focus on getting stuff done quickly with the highest amount quality possible.</p>
                   <div className="bubble left"></div>
                 </div>
               </div>
+              <div className="col-sm-6">
+                <img className="work-img" src={WorkImg} alt="" />
+              </div>
             </div>
           </section>
           <section>
             <div className="row">
-              <div className="col-sm-6">
-              </div>
-              <div className="col-sm-6">
-                <div className="bubble-container right">
+              <div className="col-sm-12">
+                <div className="center-section">
+                  <div className="img-icon blue">
+                    <img className="logo-icon" src={LogoIcon} alt="" />
+                  </div>
                   <h2>Apps are out. Chatbots are in.</h2>
                   <p>The app market is over saturated in unengaging apps. Everyone is building apps whether it's movie theatre, a stadium a train station, or an airport. The answer to every solution shouldn't be: "Let's build an app for that".</p>
                   <p>That's where we come in to play. Chatbots are perfect for this. There is no app to download, no account to create and no location to enter. Chatbots offer an experience that most closely ressembles how you interact with the world around you.</p>
-                  <div className="bubble right"></div>
                 </div>
               </div>
             </div>
