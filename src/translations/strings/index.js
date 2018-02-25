892ce2885a31a41410cc1078b04193ce416641b8
Justin D'Errico <justin.derrico1991@gmail.com>
2018-02-25 14:49:18 -0500

    en-us

--- src/translations/strings/index.js
+++ src/translations/strings/index.js
@@ -3,11 +3,13 @@ import { merge } from 'ramda';
 import en from './en.js';
 import fr from './fr.js';
 import enCA from './en-ca.js';
+import enUS from './en-us.js';
 import frCA from './fr-ca.js';
 
 
 const translations = {
   'en-ca': merge(en, enCA),
+  'en-us': merge(en, enUS),
   'fr-ca': merge(fr, frCA),
 };
 
