f165fccddf5a6de1f450957bf1b909bc6ca09745
Sébastien Audet <seb@Sebastiens-MacBook-Pro.local>
2018-02-27 17:46:49 -0500

    Homepage fix

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import asyncComponent from "./components/AsyncComponent/";
 
 const AsyncHome = asyncComponent(() => import("./containers/Home/"));
-const AsyncFeatures = asyncComponent(() => import("./containers/Features/"));
 const AsyncCompany = asyncComponent(() => import("./containers/Company/"));
 const AsyncContact = asyncComponent(() => import("./containers/Contact/"));
 const AsyncNotFound = asyncComponent(() => import("./containers/NotFound/"));
