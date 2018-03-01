364f33552ad0b0f8860dbcb9571f9b94dede8556
Sébastien Audet <seb@Sebastiens-MacBook-Pro.local>
2018-03-01 10:29:42 -0500

    Remove link from footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import "./Footer.css";
 import Logo from '../../lib/img/Logo.js';
 import { getTranslations } from '../../redux/selectors/translations';
