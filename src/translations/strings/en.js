459f203558cac7b9e4bcb29315ae65b8cc41f945
Sébastien Audet <seb.audet26@gmail.com>
2018-03-02 12:40:45 -0500

    Copy

--- src/translations/strings/en.js
+++ src/translations/strings/en.js
@@ -6,30 +6,30 @@ export default {
   },
   home: {
     intro: {
-      title: 'In-seat delivery with chat.',
-      subtitle: 'A chatbot that allows fans to easily get their food delivered to their seat without missing any of the action',
+      title: 'In-seat delivery with messenger.',
+      subtitle: 'A chatbot that allows fans to easily have their food delivered to their seat without missing any of the action',
       learnMore: 'or learn more',
     },
     body: {
       title1: 'Seamless experience for fans',
-      p1: 'Fans can effortlessly order with our chatbot. There are no apps to download, no accounts to create and no location to enter. The only thing the fan has to think about is what to eat. Just scan, chat and pay!',
+      p1: 'With reduced friction points, fans can effortlessly order. There are no apps to download, no accounts to create and no location to enter. The only thing the fan has to think about is what to eat. Just scan, chat and pay!',
       title2: 'Increased productivity and efficiency for venues',
-      p2: 'Venues lose on potential sales because fans don\'t want to miss any of the action. And during breaks, customers don\'t want to wait in line either. We help venues have a steady demand all along the event and help them reach their sales potential.',
+      p2: 'Venues lose on potential sales because fans don\'t want to miss any of the action. And during breaks, customers don\'t want to wait in line either. We help venues have a steady demand all along the event and help them reach their sales potential. Venues get all the tools necessary to satisfy their customers.',
       title3: 'More insights on your customers',
-      p3: 'Get to know your fans better. The person attending the event is very often not the one who purchased the ticket. Chatler helps venues understand their customers.',
+      p3: 'Get to know your fans better. The person attending the event is often not the one who purchased the ticket. Chatler helps venues understand their customers.',
     },
   },
   company: {
     intro: {
       title: 'We help venues increase their sales.',
-      subtitle: 'At Chatler, we make it our mission to put businesses and their customers in the same conversation.',
+      subtitle: 'At Chatler, we make it our mission to remove the barriers between venues and their customers.',
     },
     body: {
-      title1: 'We are a young startup from Montreal.',
-      p1: 'At Chatler, we make it our mission to put businesses and customers in the same conversation. We are a team of engineers and designers who focus on getting stuff done quickly with the highest amount quality possible.',
-      title2: 'Apps are out. Chatbots are in.',
-      p2l1: 'The app market is over saturated in unengaging apps. Everyone is building apps whether it\'s movie theatre, a stadium a train station, or an airport. The answer to every solution shouldn\'t be: "Let\'s build an app for that".',
-      p2l2: 'That\'s where we come in to play. Chatbots are perfect for this. There is no app to download, no account to create and no location to enter. Chatbots offer an experience that most closely ressembles how you interact with the world around you.',
+      title1: 'Who are we?',
+      p1: 'We\'re a young startup from Montreal who build products that people love. We are a team of engineers and designers who focus on getting stuff done quickly with the highest amount quality possible.',
+      title2: 'Why are we doing this?',
+      p2l1: 'Apps are out. Chatbots are in. The app market is over saturated in unengaging apps. Everyone is building an app whether it\'s a movie theatre, a train station, a stadium or an airport. The answer to every problem shouldn\'t be: "Let\'s build an app for that".',
+      p2l2: 'That\'s where we come in. Chatbots are perfect for this. There is are no apps to download, no accounts to create and no location to enter. Chatbots offer a frictionless experience that most closely ressembles how you interact with the world around us.',
     },
   },
   contact: {
@@ -53,6 +53,6 @@ export default {
     contactUs: 'Contact us',
     contactLocation: 'Montreal, Quebec, Canada',
     pages: 'Pages',
-    social: 'Social Media',
+    social: 'Social',
   },
 };
--- src/translations/strings/fr.js
+++ src/translations/strings/fr.js
@@ -11,7 +11,7 @@ export default {
       learnMore: 'ou en savoir plus',
     },
     body: {
-      title1: 'Une expérience efficace pour les fans',
+      title1: 'Une expérience sans obstacles pour les fans',
       p1: 'Vos fans peuvent commander sans effort avec notre chatbot. Il n\'y a pas d\'application à télécharger, de compte à créer, ni d\'emplacement de livraison à spécifier. La seule chose dont le fan à besoin de faire est choisir ce qu\'il veut manger ou boire.',
       title2: 'Plus grande efficacité et productivité pour les concessions',
       p2: 'Les amphithéâtres perdent des ventes potentielles parce que leurs fans ne veulent pas manquer d\'action. Pendant l\'évenement, les concessions ne voient pas beaucoup de commandes alors que pendant les pauses, il y a des files d\'attentes massives. Nous aidons les concessions à avoir une demande constante tout au long du match.',
@@ -22,14 +22,14 @@ export default {
   company: {
     intro: {
       title: 'Nous aidons nos clients à augmenter leurs ventes.',
-      subtitle: 'À Chatler, c\'est notre mission d\'aider les amphithéâtres à rejoindre les besoins de leurs clients.',
+      subtitle: 'À Chatler, c\'est notre mission d\'éliminier les obstacles de vente avec votre clientèle.',
     },
     body: {
-      title1: 'Nous sommes une jeune startup de Montréal.',
-      p1: 'Nous sommes une équipe de designers et d\'ingénieurs fixé sur développer de façon rapide avec la plus grande qualité possible.',
+      title1: 'Qui sommes-nous?',
+      p1: 'Nous sommes une startup de Montréal passionnée par les nouvelles tendances technologiques. Nous sommes une équipe de designers et d\'ingénieurs qui itèrent rapidement, tout en livrant la plus grande qualité possible.',
       title2: 'Fini les Apps. C\'est le temps des chatbots.',
-      p2l1: 'Le marché est saturé en applications mobiles qui n\'engagent pas. Tout le monde est en train de bâtir une app que ce soit un cinéma, une station de train, un stade ou encore une aéroport. Les entreprises dépenses des sommes massives d\'argent pour développer des apps que très fréquemment, peu de gens utilisent. La solution à chaque problème ne devrait pas être "bâtissons une app pour ça".',
-      p2l2: 'Voilà où nous entrons en jeu. Les chatbots sont parfait pour ça. Il n\'y a pas d\'application à télécharger, ni de compte à créer. Les chatbots offre une expérience qui ressemble le plus comment nous intéragissons avec le monde autour de nous.',
+      p2l1: 'Le marché est saturé en applications mobiles qui n\'engagent pas. Tout le monde est en train de bâtir une app que ce soit un cinéma, une station de train, un stade ou encore un aéroport. Les entreprises dépensent des sommes massives d\'argent pour développer des apps que très fréquemment, peu de gens utilisent. La solution à chaque problème ne devrait pas être "Bâtissons une app pour ça".',
+      p2l2: 'Voilà où nous entrons en jeu. Les chatbots sont idéaux. Il n\'y a pas d\'application à télécharger, ni de compte à créer. Les chatbots offrent une expérience qui ressemble le plus comment nous intéragissons avec le monde autour de nous.',
     },
   },
   contact: {
