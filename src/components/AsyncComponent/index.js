634802e658f9195a00d48f7a4d54b50e9ffde81f
Justin D'Errico <justin.derrico1991@gmail.com>
2018-08-16 19:07:06 -0400

    Allow changing locale from url

--- src/components/AsyncComponent/index.js
+++ src/components/AsyncComponent/index.js
@@ -1,4 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import queryString from "query-string";
+
+import { changeLocale } from '../../redux/actions/app';
+
+
+const mapState = () => ({});
+
+const mapActions = dispatch => ({
+  changeLocale: (language, country) => (
+    dispatch(changeLocale(language, country))
+  ),
+});
 
 export default function asyncComponent(importComponent) {
   class AsyncComponent extends Component {
@@ -13,6 +26,16 @@ export default function asyncComponent(importComponent) {
     async componentDidMount() {
       const { default: component } = await importComponent();
 
+      const parsed = queryString.parse(window.location.href.split('?')[1]);
+
+      if (parsed.locale) {
+        const split = parsed.locale.split('_');
+
+        if (split.length >= 2) {
+          this.props.changeLocale(split[0].toLowerCase(), split[1].toLowerCase());
+        }
+      }
+
       this.setState({
         component: component
       });
@@ -25,5 +48,5 @@ export default function asyncComponent(importComponent) {
     }
   }
 
-  return AsyncComponent;
+  return connect(mapState, mapActions)(AsyncComponent);
 }
